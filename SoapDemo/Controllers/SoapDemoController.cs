fc7cbf7b4d14ecad5190acdc34d16275779fdc8c
alboh123 <104097571+alboh123@users.noreply.github.com>
2023-01-23 09:52:32 +0300

    Add files via upload

---
+++ SoapDemo/Controllers/SoapDemoController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoapDemo.Controllers
+{
+    [Route("api/soap-demo")]
+    [ApiController]
+    public class SoapDemoController : ControllerBase
+    {
+        private readonly SOAPDemoSoap _demoSoap;
+
+        public SoapDemoController(SOAPDemoSoap demoSoap)
+        {
+            _demoSoap = demoSoap;
+        }
+
+        [HttpGet("add-integer")]
+        public async Task<IActionResult> AddInteger(int arg1, int arg2)
+        {
+            try
+            {
+                return Ok(await _demoSoap.AddIntegerAsync(arg1, arg2));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("divide-integer")]
+        public async Task<IActionResult> DivideInteger(int dividendo, int divisor)
+        {
+            try
+            {
+                return Ok(await _demoSoap.DivideIntegerAsync(dividendo, divisor));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("FindPersonAsync")]
+        public async Task<IActionResult> FindPersonAsync(string id)
+        {
+            try
+            {
+                return Ok(await _demoSoap.FindPersonAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("GetByNameAsync")]
+        public async Task<IActionResult> GetByNameAsync(string name)
+        {
+            try
+            {
+                return Ok(await _demoSoap.GetByNameAsync(name));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("GetListByNameAsync")]
+        public async Task<IActionResult> GetListByNameAsync(string name)
+        {
+            try
+            {
+                return Ok(await _demoSoap.GetListByNameAsync(name));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("LookupCityAsync")]
+        public async Task<IActionResult> LookupCityAsync(string zip)
+        {
+            try
+            {
+                return Ok(await _demoSoap.LookupCityAsync(zip));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("MissionAsync")]
+        public async Task<IActionResult> MissionAsync()
+        {
+            try
+            {
+                return Ok(await _demoSoap.MissionAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("QueryByNameAsync")]
+        public async Task<IActionResult> QueryByNameAsync(string name)
+        {
+            try
+            {
+                return Ok(await _demoSoap.QueryByNameAsync(name));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        
+    }
+}
---
+++ SoapDemo/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Windows;
+using SoapDemo;
+using SoapDemo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<SOAPDemoSoap, SoapDemoService>();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
